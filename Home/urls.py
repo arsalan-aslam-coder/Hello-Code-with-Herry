615fc12c960facbebeed8eb9a2c97fa9a32f42e5
test-git <dholariyarahul@gmail.com>
2021-06-07 10:51:42 +0530

    4 page created changes in Home app views.py and urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -6,4 +6,7 @@ from Home import views
 
 urlpatterns = [
     path('', views.index,name='home'),
+    path('about',views.about,name="about"),
+    path('services',views.services,name="services"),
+    path('contact',views.contact,name="contact")
 ]
