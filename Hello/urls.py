a556da84054ebbff57f45b3f83442134b22ef1f0
test-git <dholariyarahul@gmail.com>
2021-06-07 17:03:13 +0530

    Change name of admin panel and create superuser

--- Hello/urls.py
+++ Hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "Henil Web Admin"
+admin.site.site_title = "Henil Admin Portal"
+admin.site.index_title = "Welcome to Henil Web Researcher Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Home.urls'))
