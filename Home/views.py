ed8556280c42403e5e8b1038d098e16e7b8638f4
test-git <dholariyarahul@gmail.com>
2021-06-07 16:40:48 +0530

    Design Home Page using index.html templates using bootstrap

--- Home/views.py
+++ Home/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     #return HttpResponse("This is Home Page")
-    return render(request, "index.html")
+    context={
+        'var1': "Henil",
+        'var2': "Rahul"
+    }
+    return render(request, "index.html",context)
 
 def about(request):
     return HttpResponse("This is About Page")
