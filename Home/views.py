27293a51f00dfbd507a090fd4c0f87e229e0b16a
test-git <dholariyarahul@gmail.com>
2021-06-07 11:09:30 +0530

    Create static file and first template and render using render function in Home app views.py

--- Home/views.py
+++ Home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is Home Page")
+    #return HttpResponse("This is Home Page")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("This is About Page")
