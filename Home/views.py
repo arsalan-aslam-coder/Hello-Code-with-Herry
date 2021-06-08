79054bbb0af7fa598c17154a529ee9c4d2864801
test-git <dholariyarahul@gmail.com>
2021-06-08 10:11:39 +0530

    Contact Us form save to Database

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from Home.models import Contact
 # Create your views here.
 def index(request):
     #return HttpResponse("This is Home Page")
@@ -16,4 +17,12 @@ def services(request):
     return HttpResponse("This is Service Page")
 
 def contact(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        date=datetime.today()
+        contact=Contact(name=name, email=email, phone=phone,desc=desc,date=date)
+        contact.save()
     return render(request, "contact.html")
